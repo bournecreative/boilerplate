1e46a35af29894661e8daf217e5371f84d408148
Christian <bournecreative@gmail.com>
2024-05-17 20:29:02 -0500

    vite, remove boilerplate

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + TypeScript + Vite Boilerplate
+
+npm create vite@latest
+
+## Install 
+
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+
+  return (
+    <>Ready...
+    </>
+  )
+}
+
+export default App
